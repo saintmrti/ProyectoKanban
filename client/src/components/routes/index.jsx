13ad20c07231c3ab3d61309da8bcbc910f6ee567
ZoiloMM <zoilo@nuvahub.com>
2023-12-01 15:15:23 -0600

    se agrego componente /TableConsolidado

--- client/src/components/routes/index.jsx
+++ client/src/components/routes/index.jsx
@@ -10,7 +10,7 @@ import Hub from "../../components/Hub";
 import Programmer from "../../components/Programmer";
 import Production from "../../components/Production";
 import SliceHistory from "../../components/SliceHistory";
-
+import TableConsolidado from "../TableConsolidado";
 // eslint-disable-next-line react-refresh/only-export-components
 export const router = createBrowserRouter([
   {
@@ -21,7 +21,7 @@ export const router = createBrowserRouter([
         <Route path="/planeacion" element={<NavProduction />}>
           <Route path="programador" element={<Programmer />} />
           <Route path="produccion" element={<Production />} />
-          <Route path="historico" element={<SliceHistory />} />
+          <Route path="historico" element={<TableConsolidado />} />{/*<SliceHistory/>*/}
         </Route>
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
