8ecd6a4cdc933d4451aa4ac8ab97cc5724b83ef1
saintnH <saint@nuvahub.com>
2024-03-14 15:24:13 -0600

    cambiamos la conexion a la bd de prod

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-  user: "nuvahub",
-  password: "Peluche21345699",
-  database: "tepo",
-  server: "qualtia.database.windows.net",
+  user: "nuvahubadmin",
+  password: "Pastiseta2018",
+  database: "nuvahubdemo",
+  server: "nuvahubserver.database.windows.net",
   options: {
     connectTimeout: 30000,
     requestTimeout: 30000,
