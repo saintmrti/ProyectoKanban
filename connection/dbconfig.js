4f6fbfb58fd593502f05e55fdd02389e811a9c15
saintnH <saint@nuvahub.com>
2024-03-14 15:42:32 -0600

    actualizamos conexion a db en dev

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,12 +1,11 @@
 const dbconfig = {
-  user: "nuvahubadmin",
-  password: "Pastiseta2018",
-  database: "nuvahubdemo",
-  server: "nuvahubserver.database.windows.net",
+  user: "nuvahub",
+  password: "Peluche21345699",
+  database: "tepo",
+  server: "qualtia.database.windows.net",
   options: {
     connectTimeout: 30000,
     requestTimeout: 30000,
   },
 };
-
 module.exports = dbconfig;
