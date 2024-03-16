fc9e8d792cd5a856d7ee6f52fed8a3f2cf9b47a4
saintnH <saint@nuvahub.com>
2024-03-15 19:20:51 -0600

    a

--- connection/dbconfig.js
+++ connection/dbconfig.js
@@ -1,11 +1,7 @@
 const dbconfig = {
-  user: "nuvahub",
-  password: "Peluche21345699",
-  database: "tepo",
-  server: "qualtia.database.windows.net",
-  options: {
-    connectTimeout: 30000,
-    requestTimeout: 30000,
-  },
+  user: "*********",
+  password: "*********",
+  database: "*********",
+  server: "*********",
 };
 module.exports = dbconfig;
